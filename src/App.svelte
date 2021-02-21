6c2dd3a4e2f8dd2704fbcdac77c1f7b1d1694846
Sir Tim <sir.tim.626@gmail.com>
2021-02-21 14:49:29 -0800

    Get next Button

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { onMount } from "svelte";
 	import { Container, Row, Col } from "sveltestrap";
+	import { Button } from "sveltestrap";
 
 	import { getRandomDadJoke } from "./request";
 	import DJForm from "./Component/Form.svelte";
@@ -8,18 +9,26 @@
 	import DJTitle from "./Component/Title.svelte";
 
 	let joke = "";
+	const onRandomJoke = async () => {
+		try {
+			joke = await getRandomDadJoke();
+		} catch (e) {
+			alert("There was an error!");
+		}
+	};
+
 	onMount(async () => {
-		joke = await getRandomDadJoke();
+		onRandomJoke();
 	});
 </script>
 
 <main>
-
-<Container>
-	<DJTitle />
-	<DJForm />
-	<DJJoke {joke} />
-</Container>
+	<Container>
+		<DJTitle />
+		<DJForm />
+		<DJJoke {joke} />
+		<Button block on:click={onRandomJoke} color="danger">Get Next</Button>
+	</Container>
 </main>
 
 <style>
