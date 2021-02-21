20ff87c1bf4de2a6c709d15719206554d6db7d46
Sir Tim <sir.tim.626@gmail.com>
2021-02-20 23:17:24 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dad Jokes
+
+[(Dad Jokes)](https://www.youtube.com/watch?v=WhxwIE66YaU)
---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+
+<main>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
